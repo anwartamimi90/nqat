ecb2b96caab75faf5084f756ff408c33f2aea664
anwar <anwaraltamimi90@gmail.com>
2025-09-01 15:45:40 +0300

    new updates under testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="app">
+      {/* NAVBAR STARTS */}
+      <header style={{ background: "orange" }}>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <a href="">البطولات</a>
+            </li>
+            <li>
+              <a href="">مباريات اليوم</a>
+            </li>
+          </ul>
+        </nav>
+
+        <a className="logo" href="/">
+          <i class="fa-regular fa-futbol"></i>
+          نقاط
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </header>
+      {/* NAVBAR ENDS */}
+
+      {/* hero section starts */}
+      <section className="home" id="home">
+        <div className="content">
+          <h2>
+            مباريات اليوم وجدول ترتيب <span>البطولات</span>
+          </h2>
+
+          <div className="cells-container">
+            <div className="cell">
+              <p className="title">مباريات اليوم</p>
+              <p className="icon">#</p>
+            </div>
+            <div className="cell">
+              <p className="title">ترتيب البطولات</p>
+              <p className="icon">$</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* hero section ends */}
+
+      {/* about section starts */}
+      <section className="about" id="about">
+        <h2 className="heading">
+          {" "}
+          <i class="fa-regular fa-futbol"></i> About<span>Me</span>
+        </h2>
+        <div className="row">
+          <div class="image">
+            <p>image here</p>
+          </div>
+          <div className="content">
+            <p>Content here</p>
+          </div>
+        </div>
+      </section>
+      {/* about section ends */}
+    </div>
+  );
 }
 
-export default App
+export default App;
